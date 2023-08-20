1a01875214a621e17720ae5af6250ebc9a147791
T1mLen6 <timleng0911@gmail.com>
2023-08-20 19:33:32 -0400

    ok

--- data_visualizer.py
+++ data_visualizer.py
@@ -2,20 +2,27 @@
 
 import matplotlib.pyplot as plt
 import csv
+import numpy as np
   
 x = []
 y = []
+
   
-with open('Pouring_Data_2023-08-14 15-40-40.csv','r') as csvfile:
+with open('Pouring_Data_2023-08-14 15-14-40.csv','r') as csvfile:
     plots = csv.reader(csvfile, delimiter = ',')
       
     for row in plots:
         x.append(row[0])
-        y.append(int(row[2]))
+        y.append(row[1])
   
-plt.bar(x, y, color = 'g', width = 0.72, label = "Age")
-plt.xlabel('Names')
-plt.ylabel('Ages')
-plt.title('Ages of different persons')
-plt.legend()
+
+fig, ax = plt.subplots()
+
+#ax.fill_between(x, y1, y2, alpha=.5, linewidth=0)
+ax.plot(x, y, linewidth=2)
+
+#ax.set(xlim=(0, 8), xticks=np.arange(1, 8), ylim=(0, 8), yticks=np.arange(1, 8))
+plt.xlabel("X-axis (Time [s])")
+plt.ylabel("Y-axis (Percentage %)")
+plt.grid()
 plt.show()
