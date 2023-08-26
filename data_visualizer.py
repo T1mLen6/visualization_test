3d5971d101a695389bd16c4c4c76260ffd9e36f9
Letian Leng <timleng0911@gmail.com>
2023-08-25 22:57:27 -0400

    big changes

--- data_visualizer.py
+++ data_visualizer.py
@@ -3,39 +3,70 @@
 import matplotlib.pyplot as plt
 import csv
 import numpy as np
-from scripy import interpolate
- 
+from scipy import interpolate
+
+
+def tolerant_mean(arrs):
+    lens = [len(i) for i in arrs]
+    arr = np.ma.empty((np.max(lens),len(arrs)))
+    arr.mask = True
+    for idx, l in enumerate(arrs):
+        arr[:len(l),idx] = l
+    return arr.mean(axis = -1), arr.std(axis=-1)
+
+
+
 x = []
 y = []
-
   
-with open('Pouring_Data_2023-08-14 15-14-40.csv','r') as csvfile:
+with open('sorted/30/Pouring_Data_2023-08-20 19-57-22.csv','r') as csvfile:
     plots = csv.reader(csvfile, delimiter = ',')
       
     for row in plots:
-        if row[3] != 0:
-            print(row)
-            x.append(row[0])
-            y.append(row[1])
-            
-        
+        #print(type(row[8]))
   
-
-fig, ax = plt.subplots()
+        if row[8] == 'True':
+            #print(row)
+            
+            x.append(round(float(row[0]),2))
+            y.append(float(row[2]))          
+  
+print(x)
+x_array = np.array(x) - x[0]
+y_array = np.array(y)
+fig, ax = plt.subplots(2)
 
 #ax.fill_between(x, y1, y2, alpha=.5, linewidth=0)
-ax.plot(x, y, label='Liquid', color ='blue')
-ax.set_xticks(ax.get_xticks()[::90])
-ax.set_yticks(ax.get_yticks()[::50])
+ax[0].plot(x_array, y_array, label='Liquid', color ='blue')
+#ax[0].set_xticks(ax.get_xticks()[::1])
+#ax[0].set_yticks(ax.get_yticks()[::1])
+ax[0].set_xlim(0,)
 
 #Spline
-x_new = np.linspace(x[0],x[-1],100)
-bspline = 
+tck = interpolate.splrep(x_array, y_array, s=0, k=3) 
+x_new = np.linspace(min(x_array), max(x_array), 75)
+y_fit = interpolate.BSpline(*tck)(x_new)
+
+#x_new = np.linspace(x[0],x[-1],100)
+#bspline = 
+ax[1].plot(x_new, y_fit, label='Liquid', color ='red')
+#ax[1].set_xticks(ax.get_xticks()[::1])
+#ax[1].set_yticks(ax.get_yticks()[::1])
+ax[1].set_xlim(0,)
 
 #ax.set(xlim=(0, 8), xticks=np.arange(1, 8), ylim=(0, 8), yticks=np.arange(1, 8))
-plt.xlabel("X-axis (Time [s])")
-plt.ylabel("Y-axis (Percentage %)")
-plt.title('Robot Real-time Pouring Data')
-plt.grid(color='silver', linestyle='-', linewidth=1)
-plt.legend()
+ax[0].set(xlabel = "X-axis (Time [s])", ylabel = "Y-axis (Percentage %)")
+ax[1].set(xlabel = "X-axis (Time [s])", ylabel = "Y-axis (Percentage %)")
+#ax.set_ylabel("Y-axis (Percentage %)")
+ax[0].set_title('Robot Real-time Pouring Data')
+ax[1].set_title('Robot Real-time Pouring Data (splined)')
+ax[0].grid(color='silver', linestyle='-', linewidth=1)
+ax[1].grid(color='silver', linestyle='-', linewidth=1)
+
+fig.tight_layout( h_pad=1.2)
+
+ax[0].legend()
+ax[1].legend()
 plt.show()
+
+
