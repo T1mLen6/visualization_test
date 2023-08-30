06762e8d31e2892ccc6991d1af488a09ff041c82
Letian Leng <timleng0911@gmail.com>
2023-08-29 22:53:08 -0400

    some changes

--- data_visualizer_mean.py
+++ data_visualizer_mean.py
@@ -47,7 +47,7 @@ def csv_opener(filename):
             
                 y_before = y_current
         
-        offset_factor = 150 # change for every %
+        offset_factor = 130 # change for every %
                 
         x = x[align_index - offset_factor :]
         y = y[align_index - offset_factor :]
@@ -83,14 +83,14 @@ if percent == 50:
     x_2, y_2, y_2f = csv_opener('sorted/50/selected/Pouring_Data_2023-08-20 18-05-02.csv')
     x_3, y_3, y_3f = csv_opener('sorted/50/selected/Pouring_Data_2023-08-20 18-44-34.csv')
     x_4, y_4, y_4f = csv_opener('sorted/50/selected/Pouring_Data_2023-08-20 19-39-26.csv')
-    x_5, y_5, y_5f= csv_opener('sorted/50/selected/Pouring_Data_2023-08-20 19-42-08.csv')    
+    x_5, y_5, y_5f = csv_opener('sorted/50/selected/Pouring_Data_2023-08-20 19-42-08.csv')    
 
 if percent == 60:
     x_1, y_1, y_1f = csv_opener('sorted/60/selected/Pouring_Data_2023-08-20 17-04-54.csv')
     x_2, y_2, y_2f = csv_opener('sorted/60/selected/Pouring_Data_2023-08-20 17-07-46.csv')
     x_3, y_3, y_3f = csv_opener('sorted/60/selected/Pouring_Data_2023-08-20 17-10-46.csv')
     x_4, y_4, y_4f = csv_opener('sorted/60/selected/Pouring_Data_2023-08-20 17-13-09.csv')
-    x_5, y_5, y_5f= csv_opener('sorted/60/selected/Pouring_Data_2023-08-20 17-15-28.csv')    
+    x_5, y_5, y_5f = csv_opener('sorted/60/selected/Pouring_Data_2023-08-20 17-15-28.csv')    
 
 if percent == 70:
     x_1, y_1, y_1f = csv_opener('sorted/70/selected/Pouring_Data_2023-08-20 16-46-17.csv')
@@ -125,20 +125,20 @@ y_plotf, errorf, maxf, minf = tolerant_mean(y_bigf)
 font2 = {'fontname': 'Times New Roman'}
 
 # Desired width-to-height ratio
-width_to_height_ratio = 2.2  # For example, 16:9 ratio
+width_to_height_ratio = 2.8  # For example, 16:9 ratio
 
-fig, ax = plt.subplots(figsize=(8, 8 / width_to_height_ratio))    
+fig, ax = plt.subplots(figsize=(12, 12 / width_to_height_ratio))    
     
-ax.set_xlim(-0.7,40)# use 57 for 30%, and 38 for other percentages
+ax.set_xlim(-0.3,40)# use 57 for 30%, and 38 for other percentages
 ax.set_ylim(-2,100)
 
 ax.grid(color='silver', linestyle='-', linewidth=1, alpha = 0.6)
 
-ax.plot(np.arange(x_plot[0],x_plot[-1]+0.1, 0.1) , y_plot, color='green', label='Liquid Mean')
-ax.fill_between(np.arange(x_plot[0],x_plot[-1]+0.1, 0.1), y_plot - error, y_plot + error, color='lightgreen',label='Liquid Std Dev', alpha = 0.6)
-
 ax.plot(np.arange(x_plot[0],x_plot[-1]+0.1, 0.1) , y_plotf, color='crimson', label='Foam Mean')
 ax.fill_between(np.arange(x_plot[0],x_plot[-1]+0.1, 0.1), y_plotf - errorf, y_plotf + errorf, color='pink', label='Foam Std Dev', alpha = 0.6)
+
+ax.plot(np.arange(x_plot[0],x_plot[-1]+0.1, 0.1) , y_plot, color='green', label='Liquid Mean')
+ax.fill_between(np.arange(x_plot[0],x_plot[-1]+0.1, 0.1), y_plot - error, y_plot + error, color='lightgreen',label='Liquid Std Dev', alpha = 0.6)
 
 # Set the font for tick labels
 for label in (ax.get_xticklabels() + ax.get_yticklabels()):
@@ -150,7 +150,7 @@ ax.set_xlabel("Time [s]", **font2)
 ax.set_ylabel("Percentage %", **font2)
 #ax.set_title("Coke Pouring: " + str(percent) + "%", **font2)
 
-ax.legend(prop={'family': 'Times New Roman'}, loc='lower right', bbox_to_anchor=(0.24, 0.63))
+ax.legend(prop={'family': 'Times New Roman'}, loc='lower right', bbox_to_anchor=(0.173, 0.68))
 
 
 # Save and display the plot
